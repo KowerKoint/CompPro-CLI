0458c95bb144daf891dc52712d9feba7896c06c3
KowerKoint <koewrkoint@gmail.com>
2022-10-17 15:23:28 +0900

    enable address sanitizer only if using linux

--- oj-test.sh
+++ oj-test.sh
@@ -5,7 +5,11 @@ set -eux
 dir="$(dirname $2)"
 base="$(basename $2 .cpp)"
 
-g++ -g -ftrapv -fsanitize=undefined -fsanitize=address -Wall $dir/$base.cpp -o $dir/$base -I . -I ./library -I ./ac-library -D DEBUG -std=gnu++17 -O2
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    g++ -g -ftrapv -fsanitize=undefined -fsanitize=address -Wall $dir/$base.cpp -o $dir/$base -I . -I ./library -I ./ac-library -D DEBUG -std=gnu++17 -O2
+else
+    g++ -g -ftrapv -Wall $dir/$base.cpp -o $dir/$base -I . -I ./library -I ./ac-library -D DEBUG -std=gnu++17 -O2
+fi
 
 oj-bundle $dir/$base.cpp -I . -I library -I ac-library >$dir/${base}_expanded.cpp
 
